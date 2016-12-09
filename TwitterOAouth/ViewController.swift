35227455222958958888bfdf7a3afb3d79f0e571
Seiji.Y <seiji@SeijiY-no-iMac.local>
2016-12-10 00:37:14 +0900

    first commit

---
+++ TwitterOAouth/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  TwitterOAouth
+//
+//  Created by Seiji.Y. on 2016/12/09.
+//  Copyright © 2016年 Seiji.Y. All rights reserved.
+//
+
+import UIKit
+import OAuthSwift
+
+class ViewController: UIViewController {
+    
+    var oauthswift: OAuthSwift?
+    
+    let consumerData:[String:String] =
+        ["consumerKey":"srF3VG2ZlsixhQxzOqSztl6hk",
+        "consumerSecret":"XDBK1trjiXTg7Mofcl9Ec9GzcvIXoJDmXf3jNCppjO6c2Ea5ie"]
+
+    @IBAction func twitterOuth(_ sender:UIButton) {
+        doOAuthTwitter(consumerData)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // MARK: Twitter
+    func doOAuthTwitter(_ serviceParameters: [String:String]){
+        let oauthswift = OAuth1Swift(
+            consumerKey:    serviceParameters["consumerKey"]!,
+            consumerSecret: serviceParameters["consumerSecret"]!,
+            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+        )
+        self.oauthswift = oauthswift
+        oauthswift.authorizeURLHandler = getURLHandler()
+        let _ = oauthswift.authorize(
+            withCallbackURL: URL(string: "TwitterOAuth://oauth-callback")!,
+            success: { credential, response, parameters in
+                self.showTokenAlert(name: serviceParameters["name"], credential: credential)
+        },
+            failure: { error in
+                print(error.description)
+        }
+        )
+    }
+
+    func getURLHandler() -> OAuthSwiftURLHandlerType {
+        if #available(iOS 9.0, *) {
+            let handler = SafariURLHandler(viewController: self, oauthSwift: self.oauthswift!)
+                handler.presentCompletion = {
+                print("Safari presented")
+            }
+            handler.dismissCompletion = {
+                print("Safari dismissed")
+            }
+            return handler
+        }
+        return OAuthSwiftOpenURLExternally.sharedInstance
+    }
+
+    func showTokenAlert(name: String?, credential: OAuthSwiftCredential) {
+        var message = "oauth_token:\(credential.oauthToken)"
+        if !credential.oauthTokenSecret.isEmpty {
+            message += "\n\noauth_token_secret:\(credential.oauthTokenSecret)"
+        }
+        self.showAlertView(title: name ?? "Service", message: message)
+        
+    }
+    
+    func showAlertView(title: String, message: String) {
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
